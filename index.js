fbda365b02dc76e7796d7440b42cb1cbea27dd05
Savage9193 <tyagishahvez551@gmail.com>
2024-04-04 23:12:35 +0530

    hello:

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var love = document.querySelector("i")
 con.addEventListener("dblclick" , function(){
    love.style.transform='translate(-50%,-50%) scale(1)'
    love.style.opacity=0.8
+   love.style.color="red"
 
    setTimeout(function(){
     love.style.opacity= 0
    },1000)
    setTimeout(function(){
     love.style.transform='translate(-50%,-50%) scale(0)'
-    love.style.color="red"
+    
    },2000)
 });
 
